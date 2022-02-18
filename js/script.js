ea5f07b9424f35e50cd1e4807fa2544032535538
Peekabooqq <dextropon1007@gmail.com>
2022-02-18 14:42:06 +0200

    Batmannnn

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const burger = document.querySelector('.burger')
+const navigation = document.querySelector('.navigation');
+const navigationClose= document.querySelector('navigation__close');
+burger.addEventListener('click', () => { 
+    navigation.classList.add('navigation_active');
+});
+navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('navigation_active');
+});
+
+const mute = 
+
+const sliderThumds = new Swiper('.slider-thumbs', {
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 3,
+        centeredSlides: true,
+        loopedSlides: 4,
+
+    });
+
+    sliderThumds.on('click', (swiper) => {
+        swiper.slideTo(swiper.clickedIndex)
+    })
+
+    const sliderMain = new Swiper('.slider-main', {
+        loop: true,
+        spaceBetween: 10,
+        loopedSlides: 4,
+    });
+
+    sliderThumds.controller.control = sliderMain;
+    sliderThumds.controller.control = sliderThumds;
